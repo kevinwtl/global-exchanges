421451e9d5e4b040fb721ed0792ad3118b836264
Kevin Wong <wongtinglam@gmail.com>
2022-11-28 16:38:32 +0800

    tests

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+import unittest
+from stock_exchanges import HKEX, HSI, KRX
+import pandas as pd
+
+class Test(unittest.TestCase):
+    def test_output_type(self):
+        """
+        Test that it return dataframe
+        """
+        result = HKEX.aggregated_short_directory()
+        self.assertIsInstance(result, pd.DataFrame)
+
+
+if __name__ == '__main__':
+    unittest.main()
